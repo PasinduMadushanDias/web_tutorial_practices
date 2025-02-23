387cd39d463435be161ada12b098f98faef2ed3c
PasinduMadushanDias <pmdwijegunasinghe@gmail.com>
2025-02-23 23:04:26 +0530

    portfolio web page upload

---
+++ Day_09/script.js
@@ -0,0 +1,9 @@
+// Scroll smoothly to a section when a button is clicked
+function scrollToSection(sectionId) {
+    document.querySelector(sectionId).scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+  
+  // Optional: Add form submission logic or animation effects if needed
+  
